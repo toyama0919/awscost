49c2b482ebfc8be9f57b95bf7f4ecf36aec56262
Hiroshi Toyama <toyama0919@gmail.com>
2019-12-05 11:53:49 +0900

    fix options readable.

--- src/awscost/commands.py
+++ src/awscost/commands.py
@@ -24,14 +24,58 @@ def cli(ctx, debug, profile):
 
 
 @cli.command(help='list cost explorer')
-@click.option('--granularity', '-g', type=click.Choice(['DAILY', 'MONTHLY']), default="MONTHLY", help='granularity. (default: MONTHLY)')
-@click.option('--point', '-p', type=int, default=10, help='duration. if granularity is MONTHLY, 10 month ago start. if granularity is DAILY, 10 day ago start. (default: 10)')
-@click.option('--start', callback=Validator.validate_dateformat, type=str, help='range of start day. default is 10 month ago.')
-@click.option('--end', callback=Validator.validate_dateformat, type=str, default=datetime.today().strftime("%Y-%m-%d"), help='range of end day. default is now.')
-@click.option('--tablefmt', '-t', type=str, default='simple', help='tabulate format. (default: simple)')
-@click.option('--group-by', type=click.Choice(constants.AVAILABLE_DIMENSIONS), multiple=True, default=['SERVICE'], help='group by keys. (default: ["SERVICE"])')
-@click.option('--filter', type=json.loads, help='filter of dimensions. default is no filter.')
-@click.option('--metrics', type=click.Choice(constants.AVAILABLE_METRICS), default=constants.DEFAULT_METRICS, help='metrics. (default: UnblendedCost)')
+@click.option(
+    '--granularity',
+    '-g',
+    type=click.Choice(['DAILY', 'MONTHLY']),
+    default="MONTHLY",
+    help='granularity. (default: MONTHLY)'
+)
+@click.option(
+    '--point',
+    '-p',
+    type=int,
+    default=10,
+    help='duration. if granularity is MONTHLY, 10 month ago start. if granularity is DAILY, 10 day ago start. (default: 10)'
+)
+@click.option(
+    '--start',
+    callback=Validator.validate_dateformat,
+    type=str,
+    help='range of start day. default is 10 month ago.'
+)
+@click.option(
+    '--end',
+    callback=Validator.validate_dateformat,
+    type=str,
+    default=datetime.today().strftime("%Y-%m-%d"),
+    help='range of end day. default is now.'
+)
+@click.option(
+    '--tablefmt',
+    '-t',
+    type=str,
+    default='simple',
+    help='tabulate format. (default: simple)'
+)
+@click.option(
+    '--group-by',
+    type=click.Choice(constants.AVAILABLE_DIMENSIONS),
+    multiple=True,
+    default=['SERVICE'],
+    help='group by keys. (default: ["SERVICE"])'
+)
+@click.option(
+    '--filter',
+    type=json.loads,
+    help='filter of dimensions. default is no filter.'
+)
+@click.option(
+    '--metrics',
+    type=click.Choice(constants.AVAILABLE_METRICS),
+    default=constants.DEFAULT_METRICS,
+    help='metrics. (default: UnblendedCost)'
+)
 @click.pass_context
 def list_ce(ctx, granularity, point, start, end, tablefmt, group_by, filter, metrics):
     cost_explorer = CostExplorer(
@@ -49,9 +93,26 @@ def list_ce(ctx, granularity, point, start, end, tablefmt, group_by, filter, met
 
 
 @cli.command(help='list cloudwatch billing')
-@click.option('--range', '-r', type=click.Choice(['month', 'week', 'day']), required=True)
-@click.option('--tablefmt', '-t', type=str, default='simple', help='tabulate format. (default: simple)')
-@click.option('--point', '-p', type=int, default=10, help='number of data point. (default: 10)')
+@click.option(
+    '--range',
+    '-r',
+    type=click.Choice(['month', 'week', 'day']),
+    required=True
+)
+@click.option(
+    '--tablefmt',
+    '-t',
+    type=str,
+    default='simple',
+    help='tabulate format. (default: simple)'
+)
+@click.option(
+    '--point',
+    '-p',
+    type=int,
+    default=10,
+    help='number of data point. (default: 10)'
+)
 @click.pass_context
 def list_billing(ctx, range, tablefmt, point):
     billing = Billing(ctx.obj.debug, ctx.obj.profile)
