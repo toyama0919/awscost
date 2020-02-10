b3e405fc00b04f7e3aced9d4b8c7d7f5d3c350d1
Hiroshi Toyama <toyama0919@gmail.com>
2020-02-10 16:57:28 +0900

    v0.2.0.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 print(open(os.path.join(here, "README.md")).read())
-version = "0.1.0"
+version = "0.2.0"
 
 install_requires = ["tabulate", "boto3", "click>=7.0"]
 
