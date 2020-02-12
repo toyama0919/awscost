884966f0e97ef8cdc548ff000915fcc18dbfdcf8
Hiroshi Toyama <toyama0919@gmail.com>
2020-02-12 11:43:57 +0900

    fix release-ci.sh. git tag.

--- scripts/release-ci.sh
+++ scripts/release-ci.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -eu
 
+# build dist
 python setup.py sdist bdist_wheel
+
+# git tag
+git tag v$(python setup.py --version)
+git push origin --tags
+
+# pypi upload
 twine upload --verbose dist/awscost-$(python setup.py --version).tar.gz
